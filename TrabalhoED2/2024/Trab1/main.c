a5107447adc5841201b7cc1526557c6bbf1f5a92
markingcarlos <carlos.a.souza@unesp.br>
2024-10-22 22:15:58 -0300

    Inserção e remoção na arvore resolvido

--- TrabalhoED2/2024/Trab1/main.c
+++ TrabalhoED2/2024/Trab1/main.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,8 @@ typedef struct Node {
 // Declaração do protótipo da função Dividir
 Node *Dividir(Node *ArvoreB, int chave);
 
+Node *Inserir(Node *ArvoreB, int chave);
+
 Node *CriarNo() {
     Node *NovoNo = (Node *)malloc(sizeof(Node));
     NovoNo->isFolha = 1; // 1 - True
@@ -29,57 +32,97 @@ Node *CriarNo() {
 }
 
 Node *Dividir(Node *ArvoreB, int chave) {
-    // Implementação da função Dividir
+    // Cria um novo nó que será o pai
     Node *NovaArvore = CriarNo();
-    int meio = ArvoreB->numeroChave / 2;
-    int ChavePromovida = ArvoreB->Chaves[meio];
+    NovaArvore->isFolha = 0; // Será um nó interno
 
-    // Configura a nova árvore
-    NovaArvore->isFolha = ArvoreB->isFolha;
-    NovaArvore->numeroChave = 1;
-    NovaArvore->Chaves[0] = ChavePromovida;
-
-    // Divide as chaves e filhos
+    // Cria os nós filhos esquerdo e direito
     Node *FilhoEsquerdo = CriarNo();
     Node *FilhoDireito = CriarNo();
 
+    int meio = (Ordem - 1) / 2;
+    int ChavePromovida = ArvoreB->Chaves[meio];
+
+    // Copia as chaves para o filho esquerdo
     for (int i = 0; i < meio; i++) {
         FilhoEsquerdo->Chaves[i] = ArvoreB->Chaves[i];
         FilhoEsquerdo->numeroChave++;
     }
 
+    // Copia as chaves para o filho direito
     for (int i = meio + 1; i < ArvoreB->numeroChave; i++) {
-        FilhoDireito->Chaves[i - meio - 1] = ArvoreB->Chaves[i];
+        FilhoDireito->Chaves[i - (meio + 1)] = ArvoreB->Chaves[i];
         FilhoDireito->numeroChave++;
     }
 
+    // Configura o novo nó como pai dos filhos esquerdo e direito
+    NovaArvore->Chaves[0] = ChavePromovida;
+    NovaArvore->numeroChave = 1;
     NovaArvore->Filhos[0] = FilhoEsquerdo;
     NovaArvore->Filhos[1] = FilhoDireito;
+
+    // Verifica onde inserir a nova chave
+    if (chave > ChavePromovida) {
+        Inserir(FilhoDireito, chave);
+    } else {
+        Inserir(FilhoEsquerdo, chave);
+    }
 
     return NovaArvore;
 }
 
+
 Node *Inserir(Node *ArvoreB, int chave) {
+
     if (ArvoreB == NULL) {
         Node *NovaArvoreB = CriarNo();
         NovaArvoreB->Chaves[0] = chave;
         NovaArvoreB->numeroChave = 1;
         return NovaArvoreB;
     }
-    if (ArvoreB->numeroChave == Ordem - 1) {
-        printf("Divide\n");
-        ArvoreB = Dividir(ArvoreB, chave);
-        return ArvoreB;
-    } else {
+
+    // Se o nó for uma folha, insere a chave na posição correta
+    if (ArvoreB->isFolha) {
+        // Se o nó estiver cheio, precisa ser dividido
+        if (ArvoreB->numeroChave == Ordem - 1) {
+            return Dividir(ArvoreB, chave);
+        }
         int i;
         for (i = ArvoreB->numeroChave - 1; i >= 0 && chave < ArvoreB->Chaves[i]; i--) {
             ArvoreB->Chaves[i + 1] = ArvoreB->Chaves[i];
         }
         ArvoreB->Chaves[i + 1] = chave;
         ArvoreB->numeroChave++;
+
+
         return ArvoreB;
     }
+
+    // Se não for folha, localiza o filho apropriado para a inserção
+    int i;
+    for (i = 0; i < ArvoreB->numeroChave && chave > ArvoreB->Chaves[i]; i++){}
+
+    // Insere a chave no filho
+    ArvoreB->Filhos[i] = Inserir(ArvoreB->Filhos[i], chave);
+
+    // Se o filho foi dividido, deve-se lidar com a promoção da chave
+    if (ArvoreB->Filhos[i]->numeroChave == Ordem - 1) {
+        Node *NovoFilho = Dividir(ArvoreB->Filhos[i], chave);
+        // Promove a chave do novo filho para o nó atual
+        for (int j = ArvoreB->numeroChave; j > i; j--) {
+            ArvoreB->Chaves[j] = ArvoreB->Chaves[j - 1];
+            ArvoreB->Filhos[j + 1] = ArvoreB->Filhos[j];
+        }
+        ArvoreB->Chaves[i] = NovoFilho->Chaves[0];
+        ArvoreB->Filhos[i] = NovoFilho->Filhos[0];
+        ArvoreB->Filhos[i + 1] = NovoFilho->Filhos[1];
+        ArvoreB->numeroChave++;
+    }
+
+    return ArvoreB;
 }
+
+
 
 int main() {
     printf("Início\n");
@@ -89,12 +132,14 @@ int main() {
     Arvore = Inserir(Arvore, 3);
     Arvore = Inserir(Arvore, 2);
     Arvore = Inserir(Arvore, 5);
+    Arvore = Inserir(Arvore, 6);
 
-    printf("    Chave na pos 0: %d\n", Arvore->Chaves[0]);
-    // printf("    Chave na pos 1: %d\n", Arvore->Chaves[1]);
-    // printf("    Chave na pos 2: %d\n", Arvore->Chaves[2]);
-    // printf("    Chave na pos 3: %d\n", Arvore->Chaves[3]);
-    printf("    Número de Chaves: %d\n", Arvore->numeroChave);
+     printf("    Chave na pos 0: %d\n", Arvore->Filhos[1]->Chaves[2]);
+   // printf("    Chave na pos 0: %d\n", Arvore->Filhos[0]->Chaves[3]);
+    printf("    Chave na pos 1: %d\n", Arvore->Chaves[0]);
+    // // printf("    Chave na pos 2: %d\n", Arvore->Chaves[2]);
+    // // printf("    Chave na pos 3: %d\n", Arvore->Chaves[3]);
+    // printf("    Número de Chaves: %d\n", Arvore->numeroChave);
 
     printf("Final\n");
     return 0;
