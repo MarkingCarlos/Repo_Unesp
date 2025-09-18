c5ed8e53543b9dbf0d7a5401207933b13778d485
markingcarlos <carlos.a.souza@unesp.br>
2025-09-18 17:22:24 -0300

    Versão sequencial funcional

--- CompParalela/OpenMp/Projeto/main.c
+++ CompParalela/OpenMp/Projeto/main.c
@@ -1,113 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
-#define quantidadeMatriz 10
-void popularMatriz_Vetor(float Matriz[quantidadeMatriz][quantidadeMatriz], FILE *arquivo, float VetorResultados[quantidadeMatriz]);
-void popularCom(float VetorX[quantidadeMatriz], float numero);
-void metodoJacobi(float Matriz[quantidadeMatriz][quantidadeMatriz], float vetorCoeficientes[quantidadeMatriz], float vetorIteracaoAtual[quantidadeMatriz],int auxNumInte);
-void transferirVetor(float VetorRecebe[quantidadeMatriz], float VetorEnvia[quantidadeMatriz]);
 
-int main(void) {
-    FILE *arquivo;
-    float Matriz[quantidadeMatriz][quantidadeMatriz];
-    float vetorCoeficientes[quantidadeMatriz], vetorIteracaoAtual[quantidadeMatriz];
-    arquivo = fopen("../linear10.dat", "r");
+#define tamanhoMatriz 10
+#define TOL 1e-5
+#define MAX_IT 10000
 
-    if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo");
+void lerMatriz(FILE *arquivo, float Matriz[tamanhoMatriz][tamanhoMatriz], float vetorCoeficientes[tamanhoMatriz]) {
+    for (int i = 0; i < tamanhoMatriz; i++)
+        for (int j = 0; j < tamanhoMatriz; j++)
+            fscanf(arquivo, "%f", &Matriz[i][j]);
+    for (int i = 0; i < tamanhoMatriz; i++)
+        fscanf(arquivo, "%f", &vetorCoeficientes[i]);
+}
+
+void escreveSaida(const char *nomeArquivo, float x[tamanhoMatriz]) {
+    FILE *arquivoResultado = fopen(nomeArquivo, "w");
+    if (!arquivoResultado) {
+        perror("Erro ao criar arquivo de saída");
+        exit(1);
+    }
+    for (int i = 0; i < tamanhoMatriz; i++)
+        fprintf(arquivoResultado, "%.4f%c", x[i], (i == tamanhoMatriz-1) ? '\n' : ' ');
+    fclose(arquivoResultado);
+}
+
+void metodoJacobi(float A[tamanhoMatriz][tamanhoMatriz], float b[tamanhoMatriz], float x[tamanhoMatriz]) {
+
+    float novoX[tamanhoMatriz], diferenca, maiorDiferenca;
+    int it = 0;
+    #pragma omp parallel for
+    for (int i = 0; i < tamanhoMatriz; i++) x[i] = 0;
+
+    do {
+        maiorDiferenca = 0.0;
+        for (int i = 0; i < tamanhoMatriz; i++) {
+            float soma = 0.0;
+            for (int j = 0; j < tamanhoMatriz; j++) {
+                if (j != i)
+                    soma += A[i][j] * x[j];
+            }
+            novoX[i] = (b[i] - soma) / A[i][i];
+        }
+        // Calcula o maior deslocamento em relação à última iteração
+        for (int i = 0; i < tamanhoMatriz; i++) {
+            diferenca = fabs(novoX[i] - x[i]);
+
+            if (diferenca > maiorDiferenca) {
+                maiorDiferenca = diferenca;
+            }
+
+            x[i] = novoX[i];
+        }
+        it++;
+    } while (maiorDiferenca > TOL && it < MAX_IT);
+
+    printf("Iteracoes realizadas: %d\n", it);
+}
+
+int main() {
+    float Matriz[tamanhoMatriz][tamanhoMatriz], vetorCoeficientes[tamanhoMatriz], x[tamanhoMatriz];
+    FILE *arquivo = fopen("../linear10.dat", "r");
+
+    if (!arquivo) {
+        perror("Erro ao abrir arquivo de entrada");
         return 1;
     }
 
-    popularMatriz_Vetor(Matriz, arquivo, vetorCoeficientes);
-    popularCom(vetorIteracaoAtual,0);
-    metodoJacobi(Matriz,vetorCoeficientes,vetorIteracaoAtual,0);
-
-
+    lerMatriz(arquivo, Matriz, vetorCoeficientes);
     fclose(arquivo);
+
+    metodoJacobi(Matriz, vetorCoeficientes, x);
+
+    escreveSaida("saida.txt", x);
+
     return 0;
-}
-
-
-void metodoJacobi(float Matriz[quantidadeMatriz][quantidadeMatriz], float vetorCoeficientes[quantidadeMatriz], float vetorIteracaoAtual[quantidadeMatriz], int auxNumInte) {
-    float vetorAux[quantidadeMatriz];
-    int iColuna = 0;
-    for (int i = 0; i < quantidadeMatriz; i++) {
-        popularCom(vetorAux,0);
-    }
-
-    for (int iLinha = 0; iLinha < quantidadeMatriz; iLinha++) {
-        for (int indice = 0; indice < quantidadeMatriz; indice++) {
-            if (iLinha == indice) {
-
-            }
-            else {
-                vetorAux[iLinha] = vetorAux[iLinha] - Matriz[iLinha][indice] * (vetorIteracaoAtual[indice]);
-            }
-        }
-
-        // Pega o valor do X que foi isolado e passa para o outro lado
-        vetorAux[iLinha]= vetorAux[iLinha] + vetorCoeficientes[iLinha];
-
-        vetorAux[iLinha] = vetorAux[iLinha]/Matriz[iLinha][iColuna];
-
-
-        printf("Valor X%d = %.4f\n",iLinha, vetorAux[iLinha]);;
-        iColuna++;
-
-    }
-
-    float diferenca  = 1e-5;
-    int parada = 1;
-    for (int i = 0; i < quantidadeMatriz; i++) {
-        if (fabs(vetorIteracaoAtual[i] - vetorAux[i]) < diferenca) {
-            printf("A diferença entre é menor que 1e-5\n");
-            parada = 1;
-        } else {
-
-            parada = 0;
-
-        }
-    }
-
-    if (parada == 0) {
-
-        transferirVetor(vetorIteracaoAtual,vetorAux);
-        metodoJacobi(Matriz, vetorCoeficientes,vetorIteracaoAtual,1);
-    }else {
-        return;
-    }
-
-}
-
-void popularCom(float VetorX[quantidadeMatriz], float numero) {
-    for (int indice = 0 ; indice < quantidadeMatriz; indice++) {
-        VetorX[indice] = numero;
-    }
-}
-
-void transferirVetor(float VetorRecebe[quantidadeMatriz], float VetorEnvia[quantidadeMatriz]) {
-    for (int indice = 0 ; indice < quantidadeMatriz; indice++) {
-        VetorRecebe[indice] = VetorEnvia[indice];
-    }
-}
-
-
-void popularMatriz_Vetor(float Matriz[quantidadeMatriz][quantidadeMatriz], FILE *arquivo, float VetorResultados[quantidadeMatriz]) {
-    int indice_coluna = 0;
-    int indice_linha = 0;
-    for (indice_linha = 0; indice_linha < quantidadeMatriz+1; indice_linha++) {
-        if (indice_linha == quantidadeMatriz) {
-            for (indice_coluna = 0 ; indice_coluna < quantidadeMatriz; indice_coluna++) {
-                fscanf(arquivo, "%f", &VetorResultados[indice_coluna]);
-            }
-        }else {
-            for (indice_coluna = 0 ; indice_coluna < quantidadeMatriz; indice_coluna++) {
-                fscanf(arquivo, "%f", &Matriz[indice_linha][indice_coluna]);
-            }
-        }
-
-
-    }
-
 }
